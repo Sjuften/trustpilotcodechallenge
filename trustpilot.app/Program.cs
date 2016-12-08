c0d1a0bfecca4de6a6aff244af1654376a0cd878
Sjuften <martinbergpetersen@gmail.com>
2016-12-09 00:00:20 +0100

    update

--- trustpilot.app/Program.cs
+++ trustpilot.app/Program.cs
@@ -8,13 +8,13 @@ namespace trustpilot.app
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Looking for phrase");
-            var sw = new Stopwatch();
-            sw.Start();
-
             var anagram = "poultry outwits ants".Trim();
             var phrase = "4624d200580677270a54ccff86b9610e".ToUpper();
             var path = Path.Combine(Directory.GetCurrentDirectory(), "wordlist");
+
+            Console.WriteLine("Looking for phrase");
+            var sw = new Stopwatch();
+            sw.Start();
 
             var secretWord = new SecretPhrase().Find(path, anagram, phrase);
 
